b3c3313e211b081a65b5c95101053c32478fa388
Jan Sikorski <jan.sikorski@snowflake.com>
2025-03-17 10:17:57 +0100

    Fix

--- tests/streamlit/test_commands.py
+++ tests/streamlit/test_commands.py
@@ -14,71 +14,16 @@
 import shutil
 from pathlib import Path
 from textwrap import dedent
-from typing import List
 from unittest import mock
-from unittest.mock import MagicMock
 
 import pytest
-from snowflake.cli._plugins.connection.util import UIParameter
-from snowflake.core.stage import StageResource
+from streamlit.test_class import StreamlitTestClass
 
 STREAMLIT_NAME = "test_streamlit"
 TEST_WAREHOUSE = "test_warehouse"
-GET_UI_PARAMETERS = "snowflake.cli._plugins.connection.util.get_ui_parameters"
 
 
-class TestStreamlitCommands:
-    @pytest.fixture(autouse=True)
-    def setup(
-        self, mock_ctx, mock_cursor, project_directory, alter_snowflake_yml, runner
-    ):
-
-        self.ctx = mock_ctx(
-            mock_cursor(
-                rows=[
-                    {"SYSTEM$GET_SNOWSIGHT_HOST()": "https://snowsight.domain"},
-                    {"CURRENT_ACCOUNT_NAME()": "https://snowsight.domain"},
-                ],
-                columns=["SYSTEM$GET_SNOWSIGHT_HOST()"],
-            )
-        )
-
-        self.mock_connector = mock.patch(
-            "snowflake.connector.connect", return_value=self.ctx
-        ).start()
-
-        self.mock_param = mock.patch(
-            GET_UI_PARAMETERS,
-            return_value={UIParameter.NA_ENABLE_REGIONLESS_REDIRECT: False},
-        ).start()
-
-        mock_stage_resource = StageResource(
-            name="stage_resource_mock", collection=MagicMock()
-        )
-
-        self.mock_create_stage = mock.patch(
-            "snowflake.cli._plugins.streamlit.streamlit_entity.StreamlitEntity._create_stage_if_not_exists",
-            return_value=mock_stage_resource,
-        ).start()
-
-        self.mock_put = mock.patch(
-            "snowflake.core.stage._stage.StageResource.put"
-        ).start()
-
-        self.mock_get_account = mock.patch(
-            "snowflake.cli._plugins.connection.util.get_account"
-        ).start()
-        self.mock_get_account.return_value = "my_account"
-
-        self.mock_streamlit_exists = mock_streamlit_exists = mock.patch(
-            "snowflake.cli._plugins.streamlit.streamlit_entity.StreamlitEntity._object_exists",
-            lambda _, **kwargs: False,
-        ).start()
-
-        self.project_directory = project_directory
-        self.alter_snowflake_yml = alter_snowflake_yml
-        self.runner = runner
-
+class TestStreamlitCommands(StreamlitTestClass):
     def test_list_streamlit(self):
         result = self.runner.invoke(["object", "list", "streamlit"])
 
@@ -742,17 +687,3 @@ class TestStreamlitCommands:
         assert self.ctx.get_queries() == [
             "EXECUTE STREAMLIT IDENTIFIER('test_streamlit')()"
         ]
-
-    def _assert_that_exactly_those_files_were_put_to_stage(
-        self,
-        put_files: List[str],
-        stage_name: str = f"@MockDatabase.MockSchema.streamlit/{STREAMLIT_NAME}",
-    ):
-        assert self.mock_put.call_count == len(put_files)
-
-        for file in put_files:  # This seems repeatable, extract it to a function
-            self.mock_put.assert_any_call(
-                local_file_name=Path(file),
-                stage_location=f"{stage_name}/{file}",
-                overwrite=True,
-            )
